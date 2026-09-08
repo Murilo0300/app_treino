a54ce5ff50745f8705db6560e9256af74ac9722b
Murilo0300 <murilo0300@hotmail.com.br>
2026-09-08 10:46:46 -0300

    adicionando firebase e gitgnore

--- js/login.js
+++ js/login.js
@@ -1,29 +1,60 @@
-import { auth } from "./firebase.js";
+import { auth, db } from "./firebase.js";
 
 import {
     signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
 
+import {
+    doc,
+    setDoc,
+    serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
 
-// TESTE TEMPORÁRIO DE LOGIN
+
+// ==========================================
+// TESTE DE LOGIN
+// ==========================================
 
 const email = prompt("Digite seu e-mail:");
 const senha = prompt("Digite sua senha:");
 
-signInWithEmailAndPassword(auth, email, senha)
-    .then((userCredential) => {
+try {
 
-        console.log("Firebase conectado!");
-        console.log("Usuário:", userCredential.user.email);
-        console.log("UID:", userCredential.user.uid);
+    const userCredential =
+        await signInWithEmailAndPassword(auth, email, senha);
 
-        alert("Login realizado com sucesso!");
+    const usuario = userCredential.user;
 
-    })
-    .catch((erro) => {
+    console.log("Login realizado!");
+    console.log("UID:", usuario.uid);
 
-        console.error("Erro no login:", erro);
 
-        alert("Erro ao fazer login: " + erro.code);
+    // ======================================
+    // TESTE DE GRAVAÇÃO NO FIRESTORE
+    // ======================================
 
-    });
+    await setDoc(
+        doc(db, "usuarios", usuario.uid, "configuracoes", "perfil"),
+        {
+            aplicativo: "Meu Treino",
+            testeFirebase: true,
+            atualizadoEm: serverTimestamp()
+        }
+    );
+
+
+    alert("Firebase conectado e Firestore funcionando!");
+
+    console.log("Documento gravado no Firestore.");
+
+}
+catch (erro) {
+
+    console.error(erro);
+
+    alert(
+        "Erro Firebase: " +
+        erro.code
+    );
+
+}
